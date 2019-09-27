a345d7b86d96e05dcc32b670c10ecabdc4417d39
liujie <liujie1@ixianlai.com>
2019-09-27 16:33:05 +0800

    Initial Commit

---
+++ LearnOpenGL-Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL-Demo
+//
+//  Created by 刘杰 on 2019/9/27.
+//  Copyright © 2019 刘杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
