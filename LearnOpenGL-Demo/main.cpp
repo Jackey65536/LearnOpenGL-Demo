67b8af3bfae3aca58655e272adb4415482f7b4c4
liujie <liujie1@ixianlai.com>
2019-09-27 17:32:36 +0800

    添加工程文件

--- LearnOpenGL-Demo/main.cpp
+++ LearnOpenGL-Demo/main.cpp
@@ -1,15 +1,41 @@
-//
-//  main.cpp
-//  LearnOpenGL-Demo
-//
-//  Created by 刘杰 on 2019/9/27.
-//  Copyright © 2019 刘杰. All rights reserved.
-//
-
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GLFWwindow* window;
+    if (!glfwInit()) {
+        return -1;
+    }
+    window = glfwCreateWindow(800, 600, "OpenGL Base Project", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if (!glewInit()) {
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    do {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        {
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex2f(0, 0.5);
+            
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex2f(-0.5, -0.5);
+            
+            glColor3f(0.0, 0.0, 1.0);
+            glVertex2f(0.5, -0.5);
+        }
+        glEnd();
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (!glfwWindowShouldClose(window));
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
     return 0;
 }
